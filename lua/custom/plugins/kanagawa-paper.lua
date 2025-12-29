e777e65ec6b765096bbbbd7ee529d75bce2dab95
Tawfeeq <tawfeeqmeeri@gmail.com>
2025-12-29 09:06:22 +0100

    kanagawa paper update red

--- lua/custom/plugins/kanagawa-paper.lua
+++ lua/custom/plugins/kanagawa-paper.lua
@@ -1,4 +1,24 @@
 return {
   'thesimonho/kanagawa-paper.nvim',
-  opts = {},
+  opts = {
+    overrides = function(colors)
+      -- local red = '#E46876' -- Kanagawa red
+      local red = '#C46A70'
+      return {
+        --------------------------------------------------
+        -- VARIABLES (ALL LANGUAGES)
+        --------------------------------------------------
+        ['@variable.member'] = { fg = red },
+        -- ['@variable.parameter'] = { fg = red },
+
+        --------------------------------------------------
+        -- RETURN KEYWORD (ALL LANGUAGES)
+        --------------------------------------------------
+        ['@keyword.return'] = { fg = red },
+      }
+    end,
+  },
+  config = function(_, opts)
+    require('kanagawa-paper').setup(opts)
+  end,
 }
